87d8b83f1e08ce290742aedb3e391f8810af06b3
David Guzman <davidguzmaninc@gmail.com>
2022-05-23 21:43:05 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <canvas className="bg"></canvas>
+      <main className="absolute">
+        <h1>Welcome to my Webpage</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
