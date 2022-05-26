9aa1d26f7bdafc64dedf911a1fb34ff6e99d5c8d
David Guzman <davidguzmaninc@gmail.com>
2022-05-25 23:25:18 -0400

    Updated Splash page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setShowSplash(false), 1000);
+  }, []);
   return (
     <div className="App">
-      <canvas className="bg"></canvas>
-      <main className="absolute">
-        <h1>Welcome to my Webpage</h1>
-      </main>
+      {showSplash ? (
+        <SplashPage title="Front end Developer" />
+      ) : (
+        <>
+          <canvas className="bg"></canvas>
+          <main className="absolute">
+            <h1>Welcome to my Webpage</h1>
+          </main>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/SplashPage/SplashPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SplashPage = () => {
+  return (
+    <div className="Splash Page">
+      David Guzman | Software Engineer
+    </div>
+  )
+}
+
+export default SplashPage
